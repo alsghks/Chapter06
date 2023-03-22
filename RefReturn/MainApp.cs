aae2dcd355237c331d549ac48a77a15ff0864969
민환 이 <dbjenjoy@gmail.com>
2023-03-22 15:36:10 +0900

    20230322-수업내용

---
+++ RefReturn/MainApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RefReturn
+{
+    class Product
+    {
+        private int price = 100;
+
+        public ref int GetPrice()
+        {
+            return ref price;
+        }
+            public void PrintPrice()
+        {
+            Console.WriteLine($"Price :{price}");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Product carrot = new Product();
+            ref int ref_local_price = ref carrot.GetPrice();
+            int Normal_local_price = carrot.GetPrice();
+
+            carrot.PrintPrice();
+            Console.WriteLine($"Ref Local Price : {ref_local_price}");
+            Console.WriteLine($"Normal Local Price : {Normal_local_price}");
+
+            ref_local_price = 200;
+
+            carrot.PrintPrice();
+            Console.WriteLine($"Ref Local Price : {ref_local_price}");
+            Console.WriteLine($"Normal Locla Price : {Normal_local_price}");0
+        }
+    }
+}
