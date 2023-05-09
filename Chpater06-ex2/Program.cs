5b9d5df5481bb5fe615288d5c8e98ea7bad7558b
alsghks <dbjenjoy@gmail.com>
2023-05-09 10:38:41 +0900

    Chpater06-과제

---
+++ Chpater06-ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpater06_ex2
+{
+    class Program
+    {
+            public static void Mean(double a, double b, double c, double d, double e, ref double mean)
+            {
+                mean = (a + b + c + d + e) / 5;
+            }
+
+            static void Main(string[] args)
+            {
+
+                double mean = 0;
+
+                Mean(1, 2, 3, 4, 5, ref mean);
+
+                Console.WriteLine($"평균 : {mean}");
+            }
+    }
+}
